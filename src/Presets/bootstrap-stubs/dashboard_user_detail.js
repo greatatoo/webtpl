7bc418fbdbc8bfc94e44c23a121694001f9e5e37
David Lu <greatatoo@gmail.com>
2020-12-08 17:56:18 +0800

    Refactor dashboard users

--- src/Presets/bootstrap-stubs/dashboard_user_detail.js
+++ src/Presets/bootstrap-stubs/dashboard_user_detail.js
@@ -8,6 +8,28 @@ $('.btn-user-detail-update')
     });
 
 /**
+ * Button - User Delete
+ */
+var btnUserDelete = $('.btn-user-delete');
+var confirmTimes = btnUserDelete.attr('data-confirm-times');
+$('.confirm-times', btnUserDelete).text(confirmTimes);
+
+btnUserDelete
+    .click(function () {
+        var confirmVal = parseInt($('.confirm-times', this).text(), 10);
+        if (confirmVal > 1) {
+            $('.confirm-times', this).text(confirmVal - 1);
+            return false;
+        } else if (confirmVal == 0) {
+            return false;
+        }
+
+        var userId = $(this).attr('data-user-id');
+        console.log('btnUserDelete clicked ' + userId);
+        $('.confirm-times', this).text(0);
+    });
+
+/**
  * DataTable - User's Roles
  */
 var userRolesDt = $('#dashboard-user-roles-table').DataTable({
@@ -36,18 +58,28 @@ var userRolesDt = $('#dashboard-user-roles-table').DataTable({
     "stateSave": false
 });
 
+/**
+ * Load user's roles
+ */
 $(function () {
-
     var userId = $('#dashboard-user-roles-table').attr('data-user-id');
 
-    //Get all roles
-    $.ajax({
-        url: '/role',
-        type: 'get',
-        success: function (allRoles) {
-            //Get user roles
-            //TODO should use promise instead of inner ajax
+    new Promise(function (resolve, reject) {
+        //Get all roles
+        $.ajax({
+            url: '/role',
+            type: 'get',
+            success: function (allRoles) {
+                resolve(allRoles);
+            },
+            error: function (xhr) {
+                reject(xhr.status);
+            }
+        });
+    })
+        .then((allRoles) => {
             var checkedArr = [];
+            //Get user's roles
             $.ajax({
                 url: '/user/' + userId + '/role',
                 type: 'get',
@@ -62,41 +94,85 @@ $(function () {
                         var isChecked = $.inArray(el.id, checkedArr) >= 0;
                         rowArr.push([[el.id, isChecked], [el.name, el.slug]]);
                     });
-
+                    //Render datatable
                     userRolesDt.rows.add(rowArr).draw();
                 },
                 error: function (xhr) {
                 }
             });
-        },
-        error: function (xhr) {
-
-        }
-    });
+        });
 });
 
-
+/**
+* DataTable - User's Permissions
+*/
+var userPermissionsDt = $('#dashboard-user-permissions-table').DataTable({
+    "columnDefs": [
+        {
+            "targets": 0,
+            "render": function (data, type, row, meta) {
+                var permissionId = data[0];
+                var isChecked = data[1];
+                return '<input type="checkbox" value="' + permissionId + '" ' + (isChecked ? 'checked' : '') + '>';
+            }
+        },
+        {
+            "targets": 1,
+            "render": function (data, type, row, meta) {
+                var permissionName = data[0];
+                var permissionSlug = data[1];
+                return '<span title="' + permissionSlug + '">' + permissionName + '</span';
+            }
+        }
+    ],
+    "paging": false,
+    "ordering": false,
+    "info": false,
+    "searching": false,
+    "stateSave": false
+});
 
 /**
- * Button - User Delete
+ * Load user's permissions
  */
-var btnUserDelete = $('.btn-user-delete');
-var confirmTimes = btnUserDelete.attr('data-confirm-times');
-$('.confirm-times', btnUserDelete).text(confirmTimes);
+$(function () {
+    var userId = $('#dashboard-user-permissions-table').attr('data-user-id');
 
-btnUserDelete
-    .click(function () {
-        var confirmVal = parseInt($('.confirm-times', this).text(), 10);
-        if (confirmVal > 1) {
-            $('.confirm-times', this).text(confirmVal - 1);
-            return false;
-        } else if (confirmVal == 0) {
-            return false;
-        }
+    new Promise(function (resolve, reject) {
+        //Get all permissions
+        $.ajax({
+            url: '/permission',
+            type: 'get',
+            success: function (allPermissions) {
+                resolve(allPermissions);
+            },
+            error: function (xhr) {
+                reject(xhr.status);
+            }
+        });
+    })
+        .then((allPermissions) => {
+            var checkedArr = [];
+            //Get user's permissions
+            $.ajax({
+                url: '/user/' + userId + '/permission',
+                type: 'get',
+                success: function (userPermissions) {
+                    userPermissions.forEach(function (el) {
+                        checkedArr.push(el.permission_id);
+                    });
 
-        var userId = $(this).attr('data-user-id');
-        console.log('btnUserDelete clicked ' + userId);
-        $('.confirm-times', this).text(0);
-    });
-
-
+                    //Combine 2 arraies
+                    var rowArr = [];
+                    allPermissions.forEach(function (el) {
+                        var isChecked = $.inArray(el.id, checkedArr) >= 0;
+                        rowArr.push([[el.id, isChecked], [el.name, el.slug]]);
+                    });
+                    //Render datatable
+                    userPermissionsDt.rows.add(rowArr).draw();
+                },
+                error: function (xhr) {
+                }
+            });
+        });
+});
--- src/Presets/bootstrap-stubs/dashboard_users.js
+++ src/Presets/bootstrap-stubs/dashboard_users.js
@@ -1,92 +1,5 @@
-// var render = {
-//     dtRowUserDetail: function (data) {
-//         var html = '<div class="container p-3">';//container
-
-//         //row: name
-//         html += '<div class="row mt-1">';
-//         html += '<label for="name" class="col-md-3 col-form-label col-form-label-sm text-md-right">Name</label>';
-//         html += '<div class="col-md-6">';
-//         html += '<input type="text" class="form-control form-control-sm name="name" value="' + data.name + '" required autofocus>';
-//         html += '</div>';
-//         html += '</div>';
-//         //row: email
-//         html += '<div class="row mt-1">';
-//         html += '<label for="email" class="col-md-3 col-form-label col-form-label-sm text-md-right">Email</label>';
-//         html += '<div class="col-md-6">';
-//         html += '<input type="text" class="form-control form-control-sm name="email" value="' + data.email + '" required>';
-//         html += '</div>';
-//         html += '</div>';
-//         //row: password
-//         html += '<div class="row mt-1">';
-//         html += '<label for="password" class="col-md-3 col-form-label col-form-label-sm text-md-right">Password</label>';
-//         html += '<div class="col-md-6">';
-//         html += '<input type="password" class="form-control form-control-sm name="password" value="">';
-//         html += '</div>';
-//         html += '</div>';
-//         //row: api token
-//         html += '<div class="row mt-1">';
-//         html += '<label for="api-token" class="col-md-3 col-form-label col-form-label-sm text-md-right">API Token</label>';
-//         html += '<div class="col-md-6">';
-//         html += '<input type="text" class="form-control form-control-sm name="api-token" value="' + data.api_token + '">';
-//         html += '</div>';
-//         html += '</div>';
-//         //row: active
-//         html += '<div class="row mt-1">';
-//         html += '<label for="active" class="col-md-3 col-form-label col-form-label-sm text-md-right">Active</label>';
-//         html += '<div class="col-md-6">';
-//         html += '<input type="text" class="form-control form-control-sm name="active" value="' + data.active + '">';
-//         html += '</div>';
-//         html += '</div>';
-//         //row: buttons
-//         html += '<div class="row mt-3 justify-content-md-center">';
-//         html += '<button class="btn btn-danger btn-sm btn-users-delete" data-user-id="' + data.id + '" data-user-name="' + data.name + '" data-confirm-times="3">Delete (<span class="confirm-times"></span>)</button>';
-//         html += '<button class="btn btn-primary btn-sm ml-1 btn-users-roles" data-toggle="modal" data-target="#dashboard-users-roles-modal" data-user-id="' + data.id + '" data-user-name="' + data.name + '">Roles</button>';
-//         html += '<button class="btn btn-primary btn-sm ml-1 btn-users-permissions" data-toggle="modal" data-target="#dashboard-users-permissions-modal" data-user-id="' + data.id + '" data-user-name="' + data.name + '">Permissions</button>';
-//         html += '<button class="btn btn-primary btn-sm ml-1 btn-users-update" data-user-id="' + data.id + '" data-user-name="' + data.name + '">Update</button>';
-//         html += '</div>';
-
-//         html += '</div>';//end container
-//         return html;
-//     }
-// };
-
-// var bindUsersButtons = function () {
-//     var btnUsersDelete = $('.btn-users-delete', '#dashboard-users');
-//     if (!btnUsersDelete.hasClass('users-btn-binded')) {
-//         var confirmTimes = btnUsersDelete.attr('data-confirm-times');
-//         $('.confirm-times', btnUsersDelete).text(confirmTimes);
-
-//         btnUsersDelete
-//             .addClass('users-btn-binded')
-//             .click(function () {
-//                 var confirmVal = parseInt($('.confirm-times', this).text(), 10);
-//                 if (confirmVal > 1) {
-//                     $('.confirm-times', this).text(confirmVal - 1);
-//                     return false;
-//                 } else if (confirmVal == 0) {
-//                     return false;
-//                 }
-
-//                 var userId = $(this).attr('data-user-id');
-//                 var userName = $(this).attr('data-user-name');
-//                 console.log('btnUsersDelete clicked ' + userId + ' ' + userName);
-//                 $('.confirm-times', this).text(0);
-//             });
-//     }
-
-//     var btnUsersUpdate = $('.btn-users-update', '#dashboard-users');
-//     if (!btnUsersUpdate.hasClass('users-btn-binded')) {
-//         btnUsersUpdate
-//             .addClass('users-btn-binded')
-//             .click(function () {
-//                 var userId = $(this).attr('data-user-id');
-//                 var userName = $(this).attr('data-user-name');
-//                 console.log('btnUsersUpdate clicked ' + userId + ' ' + userName);
-//             });
-//     }
-// }
-
 //https://datatables.net/examples/server_side/row_details.html
+//Tips: In server mode data table, you should have the same number columns in 'ajax.data', 'option.columns' and thead.th .
 var usersDt = $('#dashboard-users').DataTable({
     "processing": true,
     "serverSide": true,
@@ -110,12 +23,11 @@ var usersDt = $('#dashboard-users').DataTable({
     "columns": [
         {
             "data": "id",
-            "orderable": false,
             "render": function (data) {
                 return '';//TODO user icon
             }
         },
-        { "data": "account" },
+        { "data": "account"},
         { "data": "name" },
         { "data": "email" },
         {
@@ -127,7 +39,7 @@ var usersDt = $('#dashboard-users').DataTable({
         { "data": "created_at" },
     ],
     "paging": true,
-    "ordering": true,
+    "ordering": false,
     "info": true,
     "searching": true,
     "stateSave": true,
@@ -138,143 +50,6 @@ $('#dashboard-users tbody').on('click', 'tr', function () {
     var tr = $(this);
     var row = usersDt.row(tr);
     var userId = row.data().id;
-    window.location.href = "/dashboard/users/"+userId;
+
+    window.location.href = "/dashboard/users/" + userId;
 });
-
-// Array to track the ids of the details displayed rows
-// var detailRows = [];
-
-// $('#dashboard-users tbody').on('click', 'tr td.details-control', function () {
-//     var tr = $(this).closest('tr');
-//     var row = usersDt.row(tr);
-//     var idx = $.inArray(tr.attr('id'), detailRows);
-
-//     if (row.child.isShown()) {
-//         tr.removeClass('details');
-//         row.child.hide();
-
-//         // Remove from the 'open' array
-//         detailRows.splice(idx, 1);
-//     }
-//     else {
-//         tr.addClass('details');
-//         row.child(render.dtRowUserDetail(row.data())).show();
-
-//         // Add to the 'open' array
-//         if (idx === -1) {
-//             detailRows.push(tr.attr('id'));
-//         }
-
-//         bindUsersButtons();
-//     }
-// });
-
-// On each draw, loop over the `detailRows` array and show any child rows
-// usersDt.on('draw', function () {
-//     $.each(detailRows, function (i, id) {
-//         $('#' + id + ' td.details-control').trigger('click');
-//     });
-// });
-
-/**
- * User-Role Table
- */
-// var usersRolesDt = $('#dashboard-users-roles-table').DataTable({
-//     "columnDefs": [
-//         {
-//             "targets": 0,
-//             "render": function (data, type, row, meta) {
-//                 var roleId = data[0];
-//                 var isChecked = data[1];
-//                 return '<input type="checkbox" value="' + roleId + '" ' + (isChecked ? 'checked' : '') + '>';
-//             }
-//         }
-//     ],
-//     "paging": false,
-//     "ordering": false,
-//     "info": false,
-//     "searching": false,
-//     "stateSave": false
-// });
-
-// $('#dashboard-users-roles-modal')
-//     .on('show.bs.modal', function (event) {
-//         var button = $(event.relatedTarget); // Button that triggered the modal
-//         var userId = button.data('user-id'); // Extract info from data-* attributes
-//         var userName = button.data('user-name'); // Extract info from data-* attributes
-
-//         //Get all roles
-//         $.ajax({
-//             url: '/role',
-//             type: 'get',
-//             success: function (allRoles) {
-//                 //Get user roles
-//                 //TODO should use promise instead of inner ajax
-//                 var checkedArr = [];
-//                 $.ajax({
-//                     url: '/user/' + userId + '/role',
-//                     type: 'get',
-//                     success: function (userRoles) {
-//                         userRoles.forEach(function (el) {
-//                             checkedArr.push(el.role_id);
-//                         });
-
-//                         //Combine 2 arraies
-//                         var rowArr = [];
-//                         allRoles.forEach(function (el) {
-//                             var isChecked = $.inArray(el.id, checkedArr) >= 0;
-//                             rowArr.push([[el.id, isChecked], el.name, el.slug]);
-//                         });
-
-//                         usersRolesDt.rows.add(rowArr).draw();
-//                     },
-//                     error: function (xhr) {
-//                     }
-//                 });
-//             },
-//             error: function (xhr) {
-
-//             }
-//         });
-
-//         var modal = $(this);
-//         modal.find('.modal-title').text('Roles of ' + userName);
-//         modal.find('.btn-ok')
-//             .attr('data-user-id', userId)
-//             .attr('data-user-name', userName);
-//     })
-//     .on('hidden.bs.modal', function (event) {
-//         usersRolesDt.clear();
-//     });
-
-// $('#dashboard-users-roles-modal .btn-ok')
-//     .click(function () {
-//         var userId = $(this).attr('data-user-id');
-//         var userName = $(this).attr('data-user-name');
-//         console.log('update users-roles ' + userId + ' ' + userName);
-
-//         $('#dashboard-users-roles-modal').modal('hide');
-//     });
-
-// $('#dashboard-users-permissions-modal')
-//     .on('show.bs.modal', function (event) {
-//         var button = $(event.relatedTarget); // Button that triggered the modal
-//         var userId = button.data('user-id'); // Extract info from data-* attributes
-//         var userName = button.data('user-name'); // Extract info from data-* attributes
-//         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//         var modal = $(this);
-//         modal.find('.modal-title').text('Permissions of ' + userName);
-//         modal.find('.btn-ok')
-//             .attr('data-user-id', userId)
-//             .attr('data-user-name', userName);
-//     });
-
-// $('#dashboard-users-permissions-modal .btn-ok')
-//     .click(function () {
-//         var userId = $(this).attr('data-user-id');
-//         var userName = $(this).attr('data-user-name');
-//         console.log('update users-permissions ' + userId + ' ' + userName);
-
-//         $('#dashboard-users-permissions-modal').modal('hide');
-//     });
