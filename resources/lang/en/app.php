ad52285651407af41bf8cd6256f615b2b3ba8654
David Lu <greatatoo@gmail.com>
2021-01-04 18:03:44 +0800

    i18n strings

---
+++ resources/lang/en/app.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'locale' => 'Locale',
+    'home' => 'Home',
+    'account' => 'Account',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'email' => 'Email',
+    'register' => 'Register',
+    'dashboard' => 'Dashboard',
+    'users' => 'Users',
+    'roles' => 'Roles',
+    'permissions' => 'Permissions',
+];
---
+++ resources/lang/zh_TW/app.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'login' => '登入',
+    'logout' => '登出',
+    'locale' => '語系',
+    'home' => '家目錄',
+    'account' => '帳號',
+    'password' => '密碼',
+    'remember_me' => '記住這帳號',
+    'email' => 'Email',
+    'register' => '註冊',
+    'dashboard' => '管理中心',
+    'users' => '帳號管理',
+    'roles' => '群組管理',
+    'permissions' => '權限管理',
+];
